8c1ccdd257a051e411f6015d8897df1d229c652b
Juan Manuel Serrano Pérez <juanmichu@gmail.com>
2026-06-19 19:01:08 +0200

    Added translations into spanish for pagination, authentication, validation and passwords

---
+++ backend/lang/es/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de Idioma de Paginación
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma son utilizadas por la biblioteca de
+    | paginación para construir los enlaces simples de paginación. Es libre
+    | de cambiarlas a lo que desee para personalizar sus vistas y adaptarlas
+    | mejor a su aplicación.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Siguiente &raquo;',
+
+];
